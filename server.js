996a2d4f3d7a511eac71ca6527827ff9a18b5117
5usama <dhuddiusama5@gmail.com>
2026-01-11 12:37:54 +0500

    Add files via upload

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Serve static files
+app.use(express.static(path.join(__dirname)));
+
+// All routes to index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`🚀 UsamaCrash Bot running on port ${PORT}`);
+  console.log(`👉 http://localhost:${PORT}`);
+});
